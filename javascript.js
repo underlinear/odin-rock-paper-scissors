c82ee541e18372475a994a80f7779f22b716c0d5
underlinear <arduouschore@gmail.com>
2023-07-16 00:24:23 +0800

    Created UI (Unfinished)

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var player = 0, computer = 0;
 
-function getComputerChoice (){
+/*function getComputerChoice (){
     let randomizer = Math.floor(Math.random() * 3);
     return randomizer == 0 ? "Rock" : randomizer == 1 ? "Paper" : "Scissors";
 }
@@ -37,23 +37,43 @@ function playRound (computerSelection,playerSelection){
         return "Invalid Input!";
     }
 } 
-
-function game() {
-    let playerSelection = prompt("Choose your move");
-    return playRound(getComputerChoice(),playerSelection);
+*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+function game(e) {
+    //let playerSelection = prompt("Choose your move");
+    alert(e);
+    //return playRound(getComputerChoice(),playerSelection);
 }
-
-while(player<5&&computer<5)
+const h3 = document.querySelector("h3");
+let title = "Dolim's \"Rock Paper Scissors\" Project";
+let titleLetter = '';
+(async function() {
+    for (let i = 0; i < title.length; i++) {
+      titleLetter = title[i];
+      h3.textContent += titleLetter;
+      await sleep(100);
+    }
+  })();
+/*
+for(let i=0;i<title.length;i++)
 {
-    console.log(game());
+    titleLetter = title[i];
+    h3.textContent += titleLetter;
+    await sleep(1000);
 }
-
+*/
+const playerChoice = document.querySelectorAll(".option img");
+playerChoice.forEach(addEventListener('click',(e)=>game(e.target)));
+/*
 if(player==5)
 {
-    console.log("You win!");
+    //console.log("You win!");
 }
 
 else
 {
-    console.log("You lose!");
+    //console.log("You lose!");
 }
+*/
